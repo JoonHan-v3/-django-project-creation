c4f0b3528184b0115dac84acab36eee93aea12f6
Joon Han <core.editor>
2022-01-27 16:22:14 -0500

    Initial commit

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\study\schoolwork\django\classwork\django-project-creation\.venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
